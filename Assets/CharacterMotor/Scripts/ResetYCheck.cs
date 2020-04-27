1e9dab0b0d0eb553893811a19f636d7ef6ca7262
Terry Nguyen <terreh@terrehbyte.com>
2020-04-27 14:45:50 -0700

    Add reset script for falling up/down OoB

---
+++ Assets/CharacterMotor/Scripts/ResetYCheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetYCheck : MonoBehaviour
+{
+    public Vector3 resetPosition;
+    public Vector2 yBounds = new Vector2(-50, 50);
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        resetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < yBounds.x || transform.position.y > yBounds.y)
+        {
+            transform.position = resetPosition;
+        }
+    }
+}
